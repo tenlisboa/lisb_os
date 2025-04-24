fd1037db00e4feae5da2f98b4f807b57628e84ea
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2025-04-24 16:04:18 -0300

    feat: printing the test results through serial port to stdio and exiting qemu

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![no_main]
 #![reexport_test_harness_main = "test_main"]
 
+mod serial;
 mod vga_buffer;
 use core::panic::PanicInfo;
 
@@ -17,6 +18,7 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
@@ -24,9 +26,36 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[test failed]");
+    serial_println!("Error: {}", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
     }
 }
