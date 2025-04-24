1dc6556df8fc3fb11b3f7458e308ce38e00c2e24
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2025-04-23 21:24:49 -0300

    feat: implement custom test frameworks running

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
 #![no_main]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 use core::panic::PanicInfo;
@@ -8,6 +11,9 @@ use core::panic::PanicInfo;
 pub extern "C" fn _start() -> ! {
     println!("Hi there folks {}", "!!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -15,4 +21,12 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
