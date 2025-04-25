295c192eaf87940efd99fb6bac6c0f0dc270eb3f
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2025-04-24 23:11:31 -0300

    ref: create a precompilation lib.rs to globalize functions for integration testing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 #![no_std]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(lisb_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
 use core::panic::PanicInfo;
+use lisb_os::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
@@ -28,46 +27,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[test failed]");
-    serial_println!("Error: {}", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T: Fn()> Testable for T {
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
+    lisb_os::test_panic_handler(info)
 }
